fd110723559101a488617405f2ff06bac9215945
Agastya Darma <gedeagas22@gmail.com>
2017-09-05 15:53:45 +0700

    Cors Update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import Express from 'express';
 import GraphHTTP from 'express-graphql';
 import Schema from './graphql_connector';
+const cors = require('cors');
 
 // Config
 const APP_PORT = 6321;
@@ -12,26 +13,14 @@ const SECRET = 'aslkdjlkaj10830912039jlkoaiuwerasdjflkasd';
 
 
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // Pass to next layer of middleware
-  next();
-  });
+
   //Index Placeholder
   app.get('/', function (req, res) {
   res.send('Hello World!, This is a backend for IF16 Voting APP');
   });
 
 // GraphQL
-app.use('/graphql', GraphHTTP({
+app.use('/graphql',cors(), GraphHTTP({
   schema: Schema,
   pretty: true,
   graphiql: true,
