04ee2d775c9fb57a06f503f12e06f77ffb3ff6c8
Agastya Darma <gedeagas22@gmail.com>
2017-09-07 22:31:45 +0700

    Minor fix

--- logic/dropboxtoken.js
+++ logic/dropboxtoken.js
@@ -3,11 +3,11 @@ import Db from '../db';
 
 export const dropboxtoken = async (parent, args, user) => {
     const token = args.dropboxtoken;
-    console.log(user)
-    const usid = user.id;
+    console.log(user);
+    const usid = {id: user.id};
     console.log(token);
     try {
-        Db.models.user.update({ dropbox: token }, { where: { id: usid }})
+        Db.models.user.update({ dropbox: token }, { where: usid})
         
     } catch (error) {
         throw new Error(error);
