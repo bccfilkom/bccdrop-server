9223da3639821e4c4aad41fa982082ba0d271e17
Agastya Darma <gedeagas22@gmail.com>
2017-09-09 14:26:50 +0700

    Cors bug fix

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
   // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'authorization,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
   next();
   });
 
